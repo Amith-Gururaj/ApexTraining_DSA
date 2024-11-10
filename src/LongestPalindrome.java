4ad866e796a6614c2579c371ae734f338dc8ddbe
Amith.G <amithraj434@gmail.com>
2024-11-10 12:01:22 +0530

    Longest Palindrome updated Solution

---
+++ src/LongestPalindrome.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class LongestPalindrome {
+
+    public static int buildLongestPalindrome(String s){
+        int[] array = new int[58];
+        int count = 0,odd = 0;
+
+        for(char each : s.toCharArray()){
+            array[each - 'A']++;
+        }
+
+        for(int each : array){
+            if(each % 2 == 0 && each != 0){
+                count += each;
+            }else if(each % 2 == 1 && each != 1){
+                count += each -1;
+                odd = 1;
+            }else if(each == 1){
+                odd = 1;
+            }
+        }
+
+        return count + odd;
+    }
+}
